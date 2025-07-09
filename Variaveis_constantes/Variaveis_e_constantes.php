1a427872620853fb649a15627806129ff0dd1a89
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-09 11:20:05 -0300

    Aulas PHP

---
+++ Variaveis_constantes/Variaveis_e_constantes.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e constantes</title>
+</head>
+<body>
+    <?php
+    const PAIS = "Brasil";
+        $nome = "Pedro Gabriel Silva dos Santos";
+        echo "Meu nome é $nome você mora no " . PAIS;
+        /*
+        Regras para nomes identificadores: 
+            - Variáveis sempre começam com o símbolo $
+            - O segundo pode ser letra ou número
+            - Aceita caracteres
+            - Aceita caracteres da tabela ASCII a apartir de 128
+            
+        */  
+    ?>
+    
+</body>
+</html>
