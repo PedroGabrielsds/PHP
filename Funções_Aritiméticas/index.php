ca54b3fb0d78cf67825d6e5d9dba6f80b54f3d3a
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-15 11:11:04 -0300

    Mais de PHP

---
+++ Funções_Aritiméticas/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Funções Aritiméticas</title>
+    </head>
+    <body>
+        <?php
+            // abs(-200); //Valor absoluto
+            // base_convert(254, 10, 8); // converte para decimal, binário e etc...
+            // ceil(); // 
+            // floor(); // 
+            // round(); //Arrendondamento
+            // hypot(); //hipotenusa
+            // intdiv(5, 2); //Divisão inteira
+            // min(); //menor número
+            // max(); //Maior número
+            // pi(); //(M_PI)
+            // pow(); //potenciação
+            // sin(); // 
+            // cos(); //cosseno
+            // tan(); //tangente
+            // sqrt(); //Raiz quadrada
+
+            print("$resultado")
+        ?>
+    </body>
+</html>
