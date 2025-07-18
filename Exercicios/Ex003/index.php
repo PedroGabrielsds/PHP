37585550a5a9cfb2562ada9eebf1366462e47953
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-18 12:28:55 -0300

    Ex003 - PHP Moderno

--- Exercicios/Ex003/index.php
+++ Exercicios/Ex003/index.php
@@ -15,6 +15,10 @@
 
                 echo"Seus R$ " . number_format($valor, 2, ',', '.') . " equivalem a <strong>US$ " . number_format($valor_dolar, 2, ',', '.') . "</strong><br>";
                 echo"<Strong>Cotação fixa de R$5,22</strong>, informada diretamente do código!";
+                //Formatação de moedas com intercionalização!
+                //$padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+                //Biblioteca INTL (Internallization PHP)
+                //echo"Seus " . numfmt_format_currency($padrao, $valor, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $valor, "USD");
             ?>
             <a href="index.html"><button>Voltar</button></a>
         </main>
