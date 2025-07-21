a0aa978a80ea1c936d7386e44166388148de5d4d
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-21 11:17:36 -0300

    Aula 03 - PHP Moderno

---
+++ Aula 03/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teste de tipos Primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos em PHP</h1>
+    <?php
+        // 0x = hexadecimal 0b = binário 0 = Octal;
+        //$num = 010;
+        //echo "O valor da variavél é $num"
+
+        // $v = "Gustavo";
+        // var_dump($v);
+
+        // $num = (int) 3e2;//ou (integer) 3 x 10(2)
+        // //echo "O valor é $num";
+        // var_dump($num);
+
+        // $num = (float) "950";
+        // var_dump($num);
+
+        // $casado = true;
+        // print "O valor para casado $casado"
+
+        
+    ?>
+</body>
+</html>
