2cf596133cf2ebb49fdab455c4c1b3a763813bea
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-21 11:18:48 -0300

    Ex005 - PHP Moderno

--- Exercicios/Ex005/index.php
+++ Exercicios/Ex005/index.php
@@ -10,12 +10,19 @@
         <main>
             <h1>Analisador de Número Real</h1>
             <?php
-                $numero = (float) $_GET["numeroreal"];
-
+                $numero = (float) $_GET["numeroreal"] ?? 0;
+                $parte_fracinaria = $numero - ((int)$numero);
                 echo "Analisando o número <strong>$numero</strong> informado pelo usuário: <br>";
-                echo "<li>A parte inteira do número é <strong>" . floor((int)$numero) . "</strong></li>";
-                echo "<li>A parte fracionária do número é <strong>" . ($numero - (int)$numero) . "</strong></li>";
+                echo"<ul>";
+                echo "<li>A parte <strong>inteira</strong> do número é <strong>" . floor((int)$numero) . "</strong></li><br>";
+                echo "<li>A parte <strong>fracionária</strong> do número é <strong>" . number_format($parte_fracinaria, 2, ",", ".") . "</strong></li>";
+                echo"</ul>";
+
             ?>
-        </main> 
+            <form action="index.html">
+                <input type="submit" value="Analisar Novamente">
+
+            </form>
+        </main>
     </body>
 </html>
