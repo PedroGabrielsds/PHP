ae7fba236d73b8a51c85e386807fc78fe85355e6
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-25 10:33:58 -0300

    Formulários Retroalimentados - PHP Moderno

--- Formulários_Retroalimentados/index.php
+++ Formulários_Retroalimentados/index.php
@@ -3,10 +3,28 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
+        <title>Formulários Retroalimentados</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        
+        <main>
+            <form action="/Formulários_Retroalimentados/index.php" method="$_GET">
+                <label for="valor1">Digite um valor: </label>
+                <input type="text" name="valor1" id="idvalor1">
+                <label for="valor2">Digite o segundo valor: </label>
+                <input type="text" name="valor2" id="idvalor2">
+
+                <input type="submit" name="enviar" id="idenviar">
+            </form>
+            <?php
+                $valor_um = $_GET['valor1'];
+                $valor_dois = $_GET["valor2"];
+                $soma = $valor_um + $valor_dois;
+
+                
+
+
+            ?>
+        </main>
     </body>
 </html>
