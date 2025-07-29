0049a76191f625e3af5ac92f3e5bf09ac84241a5
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-29 17:44:24 -0300

    Ex007 - PHP Moderno

--- Exercicios/Ex007/Ex007.php
+++ Exercicios/Ex007/Ex007.php
@@ -4,20 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!-- Faça um programa que informe quantos salários minimos existem dentro de um valor!-->
-    <?php
-        $url = "http://ipeadata.gov.br/api/odata4/Metadados?$filter=SERIE_CODIGO eq 'MTE12_SALMIN12'&$format=json
-;
+     <main>
+        <?php
+            $salario = $_REQUEST['salario'] ?? 0;
+            $salario_minimo = 1518;
+        
+        ?>
+        <h2>Informe seu salário!</h2>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="salario">Salário (R$)</label>
+            <input type="number" step="0.001" name="salario" value="<?=$salario?>">
+            <p>Considerando o salário mínimo de <strong>R$<?=number_format($salario_minimo, 2, ',', '.')?></strong></p>
+            <input type="submit" value="Calcular">
 
+        </form>
+    </main>
+    <section id="Resultado"> 
+            <h2>Resultado Final</h2>
+            <?php
+                $qtd_salarios = intdiv($salario, $salario_minimo);
+                $resto = $salario % $salario_minimo;
 
-        $conteudo = file_get_contents($url);
-
-        $dados = json_decode($conteudo, true);
-
-        var_dump($dados);
-    
-    ?>
+                echo "<p>Quem recebe um salário de R$", \number_format($salario, 2, ',', '.'), " ganha <strong>$qtd_salarios salários mínimos </strong> + R$", number_format($resto, 2, ',', '.') ,"</p>";
+        
+            ?>
+        </section>
 </body>
 </html>
