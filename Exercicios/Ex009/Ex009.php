2c7676a9eba72f1401a981ce7647c6b36dc3df14
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-07-29 17:44:47 -0300

    Ex009 - PHP Moderno

--- Exercicios/Ex009/Ex009.php
+++ Exercicios/Ex009/Ex009.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!--Faça um programa que mostre a média Aritimética Simpes e Ponderada de dois números -->
-</body>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <!--Faça um programa que mostre a média Aritmética Simpes e Ponderada de dois números -->
+        
+
+    </body>
 </html>
