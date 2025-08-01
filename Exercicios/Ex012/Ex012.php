87b285f0e3aaee20b147a9ba361207d0f01215ab
Mack <pedrogabrielsilva.d.s@gmail.com>
2025-08-01 17:26:51 -0300

    Ex011 - PHP Moderno

--- Exercicios/Ex012/Ex012.php
+++ Exercicios/Ex012/Ex012.php
@@ -9,12 +9,43 @@
     <body>
         <!--Faça um programa que leia um valor e diga quanto tempo o valor corresponde -->
         <main>
+            <?php 
+                $segundos = $_REQUEST['tempo'] ?? 00;
+            ?>
+            <h1>Calculadora de tempo</h1>
+            <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+                <label for="tempo">Qual é o total de segundos? </label>
+                <input type="number" step="0.001" name="tempo" value="<?=$segundos?>">
+                <input type="submit" value="Calcular">
 
-
+            </form>
         </main>
-        <section>
+        <section id="Resultado">
+            <h2>Calculando Segundos...</h2>
+            <?php 
+                
 
+                $semanas = floor($segundos / 604800);
+                $resto = $segundos % 604800;
 
+                $dias = floor($resto / 86400);
+                $resto = $resto % 86400;
+
+                $horas = floor($resto / 3600);
+                $resto = $resto % 3600;
+
+                $minutos = floor($resto / 60);
+                $segundos_restantes = $resto % 60;
+
+            ?>
+            <p>Analisando o valor que você digitou, <strong><?=number_format($segundos, 0, ',', '.')?> segundos</strong> equivalem a um total de: </p>
+            <ul>
+                <li><?=number_format($semanas, 0)?> semanas</li>
+                <li><?=number_format($dias, 0)?> dias</li>
+                <li><?=number_format($horas, 0)?> horas</li>
+                <li><?=number_format($minutos, 0)?> minutos</li>
+                <li><?=number_format($segundos_restantes) ?> segundos</li>
+            </ul>
         </section>
     </body>
 </html>
